53e03ac727ad57e74f1c87a5105be6f34c50d1e3
code-hasaki <71576322+code-hasaki@users.noreply.github.com>
2022-09-30 00:10:06 +0800

    Update config.js

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ const {CookieAccessInfo, CookieJar, Cookie} = cookiejar
 
 let config = {
     httpprefix: 'https', port: 443,
-    serverName: 'hasaki-proxy.now.sh',
+    serverName: 'hasaki-proxy.vercel.app',
 }
 let blockedSites = ['www.chase.com'] // accessing to chase.com was reported by google
 
